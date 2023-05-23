89660d521a90560ebf2f6fce13f0c13d2622a7f5
zhisenyang <zhisenyang@qq.com>
2023-05-23 18:19:56 +0800

    1

---
+++ src/test/vue3/slot/sub.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!--  slot header -->
+    <div>
+      <!--  slot default-->
+    </div>
+    <!--  slot footer -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// 这是子组件，实现三个slot
+</script>
