fc790de8062bde44559d986e2204b2c62e345701
zhisenyang <zhisenyang@qq.com>
2023-07-11 23:38:39 +0800

    1

--- src/test/promise/sleep.js
+++ src/test/promise/sleep.js
@@ -2,9 +2,9 @@
 // function sleep
 
 // 使main内sleep 1秒
-async function main() {
+function main() {
   console.log('start', Date.now());
-  // sleep 1秒
+  // 在这里sleep 1秒
   console.log('end', Date.now());
 }
 
--- src/test/ts/c.ts
+++ src/test/ts/c.ts
@@ -1,7 +1,7 @@
 // 类型断言
 
 // 用断言修复错误
-function main(value: string | undefined | null) {
+export function main(value: string | undefined | null) {
   const str: string = value;
   const length: number = value.length;
   console.log(str, length);
